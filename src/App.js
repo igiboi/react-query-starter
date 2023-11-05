f475dcc918be3231033eeff6227b95a863df4061
Luigi Matias <luigi.matias@codehunter.com>
2023-11-05 15:05:38 -0500

    devTools

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import {
-  QueryClientProvider,
-  QueryClient,
-  ReactQueryDevTools,
-} from "react-query";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import "./App.css";
 import { HomePage } from "./components/Home.page";
 import { RQSuperHeroesPage } from "./components/RQSuperHeroes.page";
@@ -42,7 +39,7 @@ function App() {
           </Switch>
         </div>
       </Router>
-      <ReactQueryDevTools />
+      <ReactQueryDevtools initialsIsOpen={false} position="bottom-left" />
     </QueryClientProvider>
   );
 }
--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -2,7 +2,7 @@ import { useQuery } from "react-query";
 import axios from "axios";
 
 const fetchSuperHeroes = () => {
-  return axios.get("http://localhost:4000/superheroes2");
+  return axios.get("http://localhost:4000/superheroes");
 };
 
 export const RQSuperHeroesPage = () => {
